5cffa73f105479a4fb99a3edb95d03aae2c4585f
rvennam <vennamshanker@gmail.com>
2017-09-24 14:58:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/rvennam/admobapisample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rvennam.admobapisample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
