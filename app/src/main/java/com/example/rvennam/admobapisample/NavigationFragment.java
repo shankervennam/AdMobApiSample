10768188367088f456a8dab07158d669a637d477
rvennam <vennamshanker@gmail.com>
2017-09-24 17:35:38 -0700

    Added ActionBar

--- app/src/main/java/com/example/rvennam/admobapisample/NavigationFragment.java
+++ app/src/main/java/com/example/rvennam/admobapisample/NavigationFragment.java
@@ -1,13 +1,21 @@
 package com.example.rvennam.admobapisample;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,9 +27,14 @@ public class NavigationFragment extends Fragment
     private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer";
     private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
 
-    private ListView mDrawerListView;
+    private NavigationDrawerCallbacks mCallbacks;
+    private ActionBarDrawerToggle mDrawerToggle;
 
-    private int mCurrentSelectedPosition;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
+    private View mFragmentContainerView;
+
+    private int mCurrentSelectedPosition = 0;
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
 
@@ -71,22 +84,130 @@ public class NavigationFragment extends Fragment
                 getResources().getStringArray(R.array.fragment_names)));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
-
-
     }
 
-//    private ActionBar getActionBar()
-//    {
-//        return ((AppCompatActivity) getActivity()).getSupportActionBar();
-//    }
+    public boolean isDrawerOpen()
+    {
+        return mDrawerLayout !=null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+    }
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout)
+    {
+        mFragmentContainerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+
+        //TODO
+        //mDrawerLayout.setDrawerShadow(R.drawable);
+
+        android.support.v7.app.ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        //mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, R.drawab)
+
+    }
 
     private android.support.v7.app.ActionBar getActionBar()
     {
         return ((AppCompatActivity) getActivity()).getSupportActionBar();
     }
 
-    private void selectItem(int mCurrentSelectedPosition)
+    private void selectItem(int position)
     {
+        mCurrentSelectedPosition = position;
+        if(mDrawerListView != null)
+        {
+            mDrawerListView.setItemChecked(position, true);
+        }
+        if(mDrawerLayout != null)
+        {
+            mDrawerLayout.closeDrawer(mFragmentContainerView);
+        }
 
+        if(mCallbacks != null)
+        {
+            mCallbacks.onNavigationDrawerItemSelected(position);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+
+        Activity a= null;
+
+        if(context instanceof Activity)
+        {
+            a = (Activity) context;
+        }
+
+        try
+        {
+            mCallbacks = (NavigationDrawerCallbacks) a;
+        }
+        catch (ClassCastException e)
+        {
+            throw new ClassCastException("Activity must implement Navigational Drawbacks");
+        }
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        if(mDrawerLayout != null && isDrawerOpen())
+        {
+            inflater.inflate(R.menu.global, menu);
+            showGlobalContextActionBar();
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void showGlobalContextActionBar()
+    {
+        android.support.v7.app.ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setNavigationMode(android.support.v7.app.ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setTitle("Mobile Ads API Demos");
+    }
+
+    public static interface NavigationDrawerCallbacks
+    {
+        void onNavigationDrawerItemSelected(int position);
     }
 }
